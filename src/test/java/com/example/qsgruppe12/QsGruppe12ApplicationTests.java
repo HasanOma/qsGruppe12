c0aabaa7f2ee4c886ff28558c467e93ad3e8c8b1
hasanro <hasanro@stud.ntnu.no>
2022-03-25 15:49:10 +0100

    git init

---
+++ src/test/java/com/example/qsgruppe12/QsGruppe12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qsgruppe12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QsGruppe12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

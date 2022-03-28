49c037bb44d46cf60d52e3977513ed38a86f69b4
Anders Tellefsen <anders1.tellefsen@gmail.com>
2022-03-28 14:50:27 +0200

    Added base component for queue.

---
+++ frontend/src/components/Queue.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="wrapper">
+        <div class="d-flex flex-column" id="content-wrapper">
+            <div id="content">
+                <div class="container-fluid">
+                    <h3 class="text-dark mb-4">Team</h3>
+                    <div class="card shadow">
+                        <div class="card-header py-3">
+                            <p class="text-primary m-0 fw-bold text-left">KØ</p>
+                            <p class="text-primary m-0 fw-bold text-left">Melding fra LA:&nbsp;</p>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
+                                <table class="table my-0" id="dataTable">
+                                    <thead>
+                                        <tr>
+                                            <th>Navn</th>
+                                            <th>Rom/Bord</th>
+                                            <th>Øving/Type</th>
+                                            <th>Tid i kø</th>
+                                            <th style="width: 137.125px;">Melding</th>
+                                            <th style="width: 116.7344px;"></th>
+                                            <th style="width: 116.7344px;">
+                                                <strong>Rediger</strong><br>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Angelica Ramos</td>
+                                            <td>A4-124/1</td>
+                                            <td>1/Godkjenning</td>
+                                            <td>1:00</td>
+                                            <td><br></td>
+                                            <td><br></td>
+                                            <td>
+                                                <i class="fa fa-edit" style="width: 23px;height: 16px;"></i><br>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                    <tfoot>
+                                    <tr></tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Queue",
+    props: {
+        inQueue: {
+            type: Array
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

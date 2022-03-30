09376a38476837e8673a146aa1ca7088810218e1
Brage <bragmin@hotmail.com>
2022-03-30 11:27:58 +0200

    added a method for checking exam status

--- src/main/java/com/example/qsgruppe12/service/CourseService.java
+++ src/main/java/com/example/qsgruppe12/service/CourseService.java
@@ -1,12 +1,16 @@
 package com.example.qsgruppe12.service;
 
 import com.example.qsgruppe12.dto.CourseDto;
+import com.example.qsgruppe12.model.Course;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface CourseService {
     CourseDto createCourse(CourseDto courseRegisterDto);
 
+    CourseDto update();
 
-    void deleteCourse(String code, String Semester);
+    void deleteCourse(Long courseId);
+
+    void checkExamStatus(Long courseId);
 }

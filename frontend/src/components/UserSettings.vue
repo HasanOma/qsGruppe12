b7a3ba7c6f1e0e1560719d38e60cd14eaa0b961c
Anders Tellefsen <anders1.tellefsen@gmail.com>
2022-03-31 21:26:43 +0200

    Added form validation and changed all inputs to base input

--- frontend/src/components/UserSettings.vue
+++ frontend/src/components/UserSettings.vue
@@ -3,20 +3,22 @@
     <div class="col-lg-12 d-lg-flex justify-content-lg-center">
       <div class="card shadow mb-3">
         <div class="card-header py-3">
-          <p class="text-primary m-0 fw-bold">User Settings</p>
+          <p class="text-primary m-0 fw-bold">Instillinger</p>
         </div>
         <div class="card-body">
-          <form>
+          <form @submit.prevent="onSubmit()">
             <div class="row">
               <div class="col">
                 <div class="form-group mb-3">
-                  <label class="form-label"><strong>Epost</strong></label>
-                  <input
-                    class="form-control"
-                    type="email"
-                    placeholder="user@example.com"
-                    name="email"
-                    readonly=""
+                  <label class="form-label">
+                    <strong>Epost</strong>
+                  </label>
+                  <BaseInputNoLabel
+                      class="form-control"
+                      type="email"
+                      :placeholder="email"
+                      name="email"
+                      readonly
                   />
                 </div>
               </div>
@@ -24,15 +26,48 @@
             <div class="row">
               <div class="col">
                 <div class="form-group mb-3">
-                  <label class="form-label"
-                    ><strong>Alternativ epost</strong></label
-                  >
-                  <input
-                    class="form-control"
-                    type="email"
-                    placeholder="user@example.com"
-                    name="email"
-                    readonly=""
+                  <label class="form-label">
+                    <strong>Alternativ epost</strong>
+                  </label>
+                  <BaseInputNoLabel
+                      class="form-control"
+                      type="email"
+                      :placeholder="alternativeEmail"
+                      name="email"
+                      v-model="state.email"
+                  />
+                  <span class="text-danger" v-if="v$.email.$error">
+                    {{ v$.email.$errors[0].$message }}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <div class="form-group mb-3">
+                  <label class="form-label">
+                    <strong>Fornavn</strong>
+                  </label>
+                  <BaseInputNoLabel
+                      class="form-control"
+                      type="text"
+                      :placeholder="firstName"
+                      name="first_name"
+                      readonly
+                  />
+                </div>
+              </div>
+              <div class="col">
+                <div class="form-group mb-3">
+                  <label class="form-label">
+                    <strong>Etternavn</strong><br/>
+                  </label>
+                  <BaseInputNoLabel
+                      class="form-control"
+                      type="text"
+                      :placeholder="lastName"
+                      name="last_name"
+                      readonly
                   />
                 </div>
               </div>
@@ -40,49 +75,37 @@
             <div class="row">
               <div class="col">
                 <div class="form-group mb-3">
-                  <label class="form-label"><strong>Fornavn</strong></label>
-                  <input
-                    class="form-control"
-                    type="text"
-                    placeholder="Ola"
-                    name="first_name"
+                  <label class="form-label">
+                    <strong>Nytt passord</strong><br/>
+                  </label>
+                  <BaseInputNoLabel
+                      class="form-control"
+                      type="password"
+                      v-model="state.password.password"
                   />
+                  <span class="text-danger" v-if="v$.password.password.$error">
+                    {{ v$.password.password.$errors[0].$message }}
+                  </span>
                 </div>
               </div>
               <div class="col">
                 <div class="form-group mb-3">
-                  <label class="form-label"
-                    ><strong>Etternavn</strong><br
-                  /></label>
-                  <input
-                    class="form-control"
-                    type="text"
-                    placeholder="Nordmann"
-                    name="last_name"
+                  <label class="form-label">
+                    <strong>Bekreft nytt passord</strong><br/>
+                  </label>
+                  <BaseInputNoLabel
+                      class="form-control"
+                      type="password"
+                      v-model="state.password.confirm"
                   />
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                <div class="form-group mb-3">
-                  <label class="form-label"
-                    ><strong>Nytt passord</strong><br
-                  /></label>
-                  <input class="form-control" type="password" />
-                </div>
-              </div>
-              <div class="col">
-                <div class="form-group mb-3">
-                  <label class="form-label"
-                    ><strong>Bekreft nytt passord</strong><br
-                  /></label>
-                  <input class="form-control" type="password" />
+                  <span class="text-danger" v-if="v$.password.confirm.$error">
+                    {{ v$.password.confirm.$errors[0].$message }}
+                  </span>
                 </div>
               </div>
             </div>
             <div class="form-group mb-3">
-              <button class="btn btn-primary btn-sm" type="submit">
+              <button class="btn btn-primary btn-sm" type="submit" :disabled="formIsValid">
                 Lagre
               </button>
             </div>
@@ -94,8 +117,63 @@
 </template>
 
 <script>
+import useValidate from "@vuelidate/core";
+import { required, email, minLength, sameAs } from "@vuelidate/validators";
+import { reactive, computed } from "vue";
+import BaseInputNoLabel from "@/components/BaseComponents/BaseInputNoLabel";
+
 export default {
   name: "StudentSettings",
+  components: {
+    BaseInputNoLabel
+  },
+  setup() {
+      const state = reactive({
+        email: '',
+        password: {
+          password: '',
+          confirm: '',
+        },
+      })
+
+      const rules = computed(() => {
+        return {
+          email: { required, email },
+          password: {
+            password: { required, minLength: minLength(6) },
+            confirm: { required, sameAs: sameAs(state.password.password) }
+          }
+        }
+      })
+
+      const v$ = useValidate(rules, state)
+
+      return { state, v$ }
+  },
+  data() {
+    return {
+      email: this.$store.getters.email,
+      alternativeEmail: this.$store.getters.altEmail,
+      firstName: this.$store.getters.firstName,
+      lastName: this.$store.getters.lastName,
+      formIsValid: false,
+      password: {
+        password: "",
+        confirm: ""
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.v$.$validate()
+      if(!this.v$.$error) {
+        console.log("Form is sent")
+      } else {
+        console.log(this.v$.$error)
+        console.log("checkpoint")
+      }
+    }
+  },
 };
 </script>
 

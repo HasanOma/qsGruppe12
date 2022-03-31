064f695af00202b350f85a954a3ec3983e0c66d7
Brage <bragmin@hotmail.com>
2022-03-31 15:47:35 +0200

    added test for courseRepository

---
+++ src/test/java/com/example/qsgruppe12/repository/CourseRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.example.qsgruppe12.repository;
+
+import com.example.qsgruppe12.model.Course;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class CourseRepositoryTest {
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @BeforeEach
+    void setUp() {
+        Course course =
+                Course.builder()
+                .id(1L)
+                .name("Fullstack applikasjonsutvikling")
+                .code("IDATT2105")
+                .semester("V22")
+                .queueActive(false)
+                .totalWork(6)
+                .rules("6_1_6")
+                .nrOfStudents(100)
+                .build();
+        entityManager.persist(entityManager.merge(course));
+    }
+
+    @Test
+    void findByCodeAndSemester() {
+        Course course = courseRepository.findByCodeAndSemester("IDATT2105", "V22").get();
+        assertEquals("Fullstack applikasjonsutvikling", course.getName());
+
+    }
+
+    @Test
+    void findById() {
+        Course course = courseRepository.findById(1L).get();
+        assertEquals("Fullstack applikasjonsutvikling", course.getName());
+    }
+
+//    @Test
+//    void deleteById(){
+//        courseRepository.deleteById(1L);
+////        assertEquals("Fullstack applikasjonsutvikling", course.getName());
+//        assertTrue(courseRepository.findById(1L).isEmpty());
+//    }
+
+    //No clue as to why this doesnt work, line 55 just cant find an element with id 1L
+
+}

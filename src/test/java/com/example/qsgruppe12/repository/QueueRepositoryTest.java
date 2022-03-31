a46960f9aaccba3390196591505dc5fe2f006e9f
Brage <bragmin@hotmail.com>
2022-03-31 16:40:02 +0200

    added tests

---
+++ src/test/java/com/example/qsgruppe12/repository/QueueRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.example.qsgruppe12.repository;
+
+import com.example.qsgruppe12.model.Course;
+import com.example.qsgruppe12.model.Queue;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class QueueRepositoryTest {
+    @Autowired
+    private QueueRepository queueRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("before each called");
+        Course course =
+                Course.builder()
+                        .id(1L)
+                        .name("Fullstack applikasjonsutvikling")
+                        .code("IDATT2105")
+                        .semester("V22")
+                        .queueActive(false)
+                        .totalWork(6)
+                        .rules("6_1_6")
+                        .nrOfStudents(100)
+                        .build();
+
+        Queue queue = Queue.builder()
+                .id(1L)
+                .course(course)
+                .build();
+
+        entityManager.persist(entityManager.merge(queue));
+    }
+    @AfterEach
+    void teardown(){
+        queueRepository.deleteAll();
+    }
+
+    @Test
+    void getByCourseId() {
+        Queue queue = queueRepository.getByCourseId(1L).get();
+        assertEquals(1L, queue.getId());
+    }
+
+    @Test
+    void getByCourseId_falseId(){
+        assertTrue(queueRepository.getByCourseId(2L).isEmpty());
+    }
+}
---
+++ src/test/java/com/example/qsgruppe12/repository/User_CourseRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.example.qsgruppe12.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class User_CourseRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void findAllByUserId() {
+    }
+
+    @Test
+    void findAllByUserIdAndCourseId() {
+    }
+
+    @Test
+    void findAllByCourseId() {
+    }
+}

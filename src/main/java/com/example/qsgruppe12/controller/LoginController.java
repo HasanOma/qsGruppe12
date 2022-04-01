880754ac4c22bf00bd12ee385ee81ae43a7c5dcb
Anders Tellefsen <anders1.tellefsen@gmail.com>
2022-04-01 15:58:39 +0200

    Added new LoginController

--- src/main/java/com/example/qsgruppe12/controller/LoginController.java
+++ src/main/java/com/example/qsgruppe12/controller/LoginController.java
@@ -1,2 +1,25 @@
-package com.example.qsgruppe12.controller;public class LoginController {
+package com.example.qsgruppe12.controller;
+
+import com.example.qsgruppe12.dto.userdtos.UserLoginDto;
+import com.example.qsgruppe12.dto.userdtos.UserLoginReturnDto;
+import com.example.qsgruppe12.service.user.UserService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/")
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public UserLoginReturnDto getUserLogin(@Valid @RequestBody UserLoginDto login){
+        return userService.getUserLoggingIn(login);
+    }
 }
